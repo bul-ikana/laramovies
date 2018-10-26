f4a4eda4f363fdf07d13fd011e21b8e4e3f2eb49
Hugo Aguirre <bul.ikana@gmail.com>
2018-10-26 13:14:23 -0500

    Define relationships between films and genres

--- app/Film.php
+++ app/Film.php
@@ -2,9 +2,18 @@
 
 namespace App;
 
+use App\Genre;
+use App\Comment;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Film extends Model
 {
-    //
+    public function genres () {
+        return $this->belongsToMany(Genre::class);
+    }
+
+    public function comments () {
+        return $this->hasMany(Comment::class);
+    }
 }
--- app/Genre.php
+++ app/Genre.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use App\Film;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Genre extends Model
 {
-    //
+    public function films () {
+        return $this->belongsToMany(Film::class);
+    }
 }
