d3eddb45f2cfccf9d396bc1a8d410942efe872ce
Hugo Aguirre <bul.ikana@gmail.com>
2018-10-26 17:18:27 -0500

    Layout for single movie page

---
+++ resources/views/movie.blade.php
@@ -0,0 +1,26 @@
+@extends('base')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-8">
+            <h1>This movie</h1>
+            <a href="https://picsum.photos/1000/1500"><img src="https://picsum.photos/2000/3000"></a>
+            <p>Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.</p>
+            <p><strong>Relase date:</strong> 12/12/12</p>
+            <p><strong>Rating:</strong> 4/5</p>
+            <p><strong>Ticket price:</strong> $12.50</p>
+            <p><strong>Country:</strong> Mexico</p>
+            <p><strong>Genre:</strong> Action</p>
+        </div>
+        <div class="col-md-4">
+            <h4>List of movies:</h4>
+            <ul class="list-group">
+                <li class="list-group-item"><a href="#">Cricket</a></li>
+                <li class="list-group-item"><a href="#">Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro.</a></li>
+                <li class="list-group-item"><a href="#">Brucks</a></li>
+            </ul>
+        </div>
+
+
+    </div>
+@endsection
