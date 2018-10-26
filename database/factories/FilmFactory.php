9dc26abcb194f56eecb4cc8348d24713a8e8d5e7
Hugo Aguirre <bul.ikana@gmail.com>
2018-10-26 15:37:37 -0500

    Update film factory to include slug

--- database/factories/FilmFactory.php
+++ database/factories/FilmFactory.php
@@ -3,13 +3,15 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Film::class, function (Faker $faker) {
+    $n = $faker->realText(100);
     return [
-        'name'          =>  $faker->realText(100),
+        'name'          =>  $n,
         'description'   =>  $faker->realText(500),
         'release_date'  =>  $faker->date(),
         'rating'        =>  $faker->numberBetween(1,5),
         'ticket_price'  =>  $faker->randomFloat(2, 0, 15),
         'country'       =>  $faker->country(),
-        'photo'         =>  $faker->imageUrl()
+        'photo'         =>  $faker->imageUrl(),
+        'slug'          =>  str_slug($n),
     ];
 });
