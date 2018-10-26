2e8d754da5ff8172ce3a200195e2bcf1d8221e43
Hugo Aguirre <bul.ikana@gmail.com>
2018-10-26 15:40:57 -0500

    Define index function for film api controller

---
+++ app/Http/Controllers/Api/FilmController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Film;
+
+class FilmController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = [];
+        $data['last_film'] = Film::with('genres')->with('comments')->latest()->first();
+        $data['all_films'] = Film::orderBy('created_at', 'desc')->pluck('name', 'slug')->all();
+        return response()->json($data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  string  $slug
+     * @return \Illuminate\Http\Response
+     */
+    public function show($slug)
+    {
+        $film = Film::where('slug', $slug)->first();
+        return response()->json($film);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $slug
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $slug)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  string  $slug
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($slug)
+    {
+        //
+    }
+}
