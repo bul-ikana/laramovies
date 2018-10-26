ae894618d09c122084c841011d2e6810edbbba11
Hugo Aguirre <bul.ikana@gmail.com>
2018-10-26 13:29:07 -0500

    Define relationships between comments, users and films.

--- app/Comment.php
+++ app/Comment.php
@@ -2,9 +2,18 @@
 
 namespace App;
 
+use App\Film;
+use App\User;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    public function film () {
+        return $this->belongsTo(Film::class);
+    }
+
+    public function user () {
+        return $this->belongsTo(User::class);
+    }
 }
