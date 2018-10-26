998777e34d2fce9f7c5d2328282e4ee6c8e8ed56
Hugo Aguirre <bul.ikana@gmail.com>
2018-10-26 15:47:51 -0500

    Test getting any movie by slug

--- tests/Feature/FilmApiTest.php
+++ tests/Feature/FilmApiTest.php
@@ -57,4 +57,42 @@ class FilmApiTest extends TestCase
             ->assertStatus(200)
             ;
     }
+
+    public function testShowRouteGetsMovieInformation () {
+        $f1 = factory(Film::class)->create();
+        $f2 = factory(Film::class)->create();
+        $f3 = factory(Film::class)->create();
+        $f4 = factory(Film::class)->create();
+
+        $response = $this->get(
+            'api/films/' . $f3->slug,
+            ["Accept" => "application/json"]
+        );
+
+        $response
+            ->assertJsonFragment(['name'        =>  $f3->name])
+            ->assertJsonFragment(['description' =>  $f3->description])
+            ->assertStatus(200)
+            ;
+    }
+
+    public function testShowRouteGetsAllFilmNamesList () {
+        $f1 = factory(Film::class)->create();
+        $f2 = factory(Film::class)->create();
+        $f3 = factory(Film::class)->create();
+        $f4 = factory(Film::class)->create();
+
+        $response = $this->get(
+            'api/films/' . $f3->slug,
+            ["Accept" => "application/json"]
+        );
+
+        $response
+            ->assertJsonFragment([$f1->slug   =>  $f1->name])
+            ->assertJsonFragment([$f2->slug   =>  $f2->name])
+            ->assertJsonFragment([$f3->slug   =>  $f3->name])
+            ->assertJsonFragment([$f4->slug   =>  $f4->name])
+            ->assertStatus(200)
+            ;
+    }
 }
