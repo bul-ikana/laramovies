e68282b49d39b07b082af28a5001e531dc70845d
Hugo Aguirre <bul.ikana@gmail.com>
2018-10-26 15:36:53 -0500

    Test index route will get a list of movie names and the information for the last film.

---
+++ tests/Feature/FilmApiTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Film;
+use App\Genre;
+use App\Comment;
+
+class FilmApiTest extends TestCase
+{
+    public function testIndexRouteGetsNewestFilmAnd () {
+        $f1 = factory(Film::class)->create();
+        sleep(1);
+        $f2 = factory(Film::class)->create();
+        sleep(1);
+        $f3 = factory(Film::class)->create();
+        $c = factory(Comment::class)->create([
+            'film_id' => $f3->id
+        ]);
+        $g = factory(Genre::class)->create();
+        $f3->genres()->attach($g);
+
+        $response = $this->get(
+            'api/films',
+            ["Accept" => "application/json"]
+        );
+
+        $response
+            ->assertJsonFragment(['name'        =>  $f3->name])
+            ->assertJsonFragment(['description' =>  $f3->description])
+            ->assertJsonFragment(['comment'     =>  $c->comment])
+            ->assertJsonFragment(['name'        =>  $g->name])
+            ->assertStatus(200)
+            ;
+    }
+
+    public function testIndexRouteGetsAllFilmNamesList () {
+        $f1 = factory(Film::class)->create();
+        $f2 = factory(Film::class)->create();
+        $f3 = factory(Film::class)->create();
+        $f4 = factory(Film::class)->create();
+
+        $response = $this->get(
+            'api/films',
+            ["Accept" => "application/json"]
+        );
+
+        $response
+            ->assertJsonFragment([$f1->slug   =>  $f1->name])
+            ->assertJsonFragment([$f2->slug   =>  $f2->name])
+            ->assertJsonFragment([$f3->slug   =>  $f3->name])
+            ->assertJsonFragment([$f4->slug   =>  $f4->name])
+            ->assertStatus(200)
+            ;
+    }
+}
