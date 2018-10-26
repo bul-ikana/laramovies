bcfeff82dca7e79c4c9454c43d5ba83b3e67d1b0
Hugo Aguirre <bul.ikana@gmail.com>
2018-10-26 17:52:01 -0500

    Load movie data from api

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -1,16 +1,23 @@
 @extends('base')
 
 @section('content')
-    <div class="row">
+    <div class="spinner" id="spinner">
+      <div class="rect1"></div>
+      <div class="rect2"></div>
+      <div class="rect3"></div>
+      <div class="rect4"></div>
+      <div class="rect5"></div>
+    </div>
+    <div class="row" id="content" style="display:none">
         <div class="col-md-8">
-            <h1>This movie</h1>
-            <a href="https://picsum.photos/1000/1500"><img src="https://picsum.photos/2000/3000"></a>
-            <p>Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.</p>
-            <p><strong>Relase date:</strong> 12/12/12</p>
-            <p><strong>Rating:</strong> 4/5</p>
-            <p><strong>Ticket price:</strong> $12.50</p>
-            <p><strong>Country:</strong> Mexico</p>
-            <p><strong>Genre:</strong> Action</p>
+            <h1 id="name"></h1>
+            <a id="link" href="#"><img id="photo" src=""></a>
+            <p id="description"></p>
+            <p><strong>Relase date:</strong> <span id="release_date"></span></p>
+            <p><strong>Rating:</strong> <span id="rating"></span>/5</p>
+            <p><strong>Ticket price:</strong> $<span id="ticket_price"></span></p>
+            <p><strong>Country:</strong> <span id="country"></span></p>
+            <p><strong id="genre_title"></strong> <span id="genre">t</span></p>
         </div>
         <div class="col-md-4">
             <h4>List of movies:</h4>
@@ -21,6 +28,28 @@
             </ul>
         </div>
 
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+        <script type="text/javascript">
+            axios
+                .get('{{ url("/api/films") }}')
+                .then( function (response) {
+                    console.log(response.data)
+
+                    document.getElementById("spinner").style.display = 'none'
+                    document.getElementById("content").style.display = 'flex'
+                    document.getElementById("name").innerHTML = response.data.last_film.name
+                    document.getElementById("photo").src = response.data.last_film.photo
+                    document.getElementById("link").href = response.data.last_film.photo
+                    document.getElementById("description").innerHTML = response.data.last_film.description
+                    document.getElementById("release_date").innerHTML = response.data.last_film.release_date
+                    document.getElementById("rating").innerHTML = response.data.last_film.rating
+                    document.getElementById("ticket_price").innerHTML = response.data.last_film.ticket_price
+                    document.getElementById("country").innerHTML = response.data.last_film.country
+                    document.getElementById("genre_title").innerHTML = response.data.last_film.genres.length ? response.data.last_film.genres.length > 1 ? "Genres:" : "Genre:" : ''
+                    document.getElementById("genre").innerHTML = response.data.last_film.genres.map(x => x.name).join(', ')
+
+                })
+        </script>
 
     </div>
 @endsection
