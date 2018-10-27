70efaf0c1504d3afeaebb54fc492e1e44ddf8fcb
Hugo Aguirre <bul.ikana@gmail.com>
2018-10-27 15:10:57 -0500

    Add default laravel auth

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('/films', 'Filmcontroller@index');
 Route::get('/films/create', 'Filmcontroller@create')->name('create');
 Route::get('/films/{slug}', 'Filmcontroller@show');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
